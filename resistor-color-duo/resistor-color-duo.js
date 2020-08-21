65fd5b4c8ac3766244a6337aca071fa63d0a1ad4
Marcos Palacios <fcoparo@gmail.com>
2020-08-21 18:06:52 +0200

    Brown and black

---
+++ resistor-color-duo/resistor-color-duo.js
@@ -0,0 +1,10 @@
+//
+// This is only a SKELETON file for the 'Resistor Color Duo' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+const COLORS = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"];
+
+export const decodedValue = (colors) => {
+  return 10;
+};
+
